3616a5b484be8aee3b2dc66499a22b251dd4eb27
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2022-03-05 16:52:28 +0530

    Delete GeekForGeeks Programs/Array Programs directory

--- GeekForGeeks Programs/Array Programs/01_Sum_Of_Array.py
+++
@@ -1,3 +0,0 @@
-arr=[12,3,4,5]
-ans=sum(arr)
-print("Sum of array is: ",ans)
--- GeekForGeeks Programs/Array Programs/02_Find_Largest_Element_In_An_Array.py
+++
@@ -1,8 +0,0 @@
-arr=[12,34,78,23,45]
-n=len(arr)
-max=arr[0]
-for i in range(1,n):
-    if arr[i]>max:
-        max=arr[i]
-
-print("Largest number in given array is: ",max)
