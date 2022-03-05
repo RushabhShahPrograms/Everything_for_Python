09c3daf82263d96c5f70e96bab98bdef29a5e7e3
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2022-03-05 16:52:37 +0530

    Delete GeekForGeeks Programs/Basic Programs directory

--- GeekForGeeks Programs/Basic Programs/02_Maximum_of_Two_Numbers.py
+++
@@ -1,6 +0,0 @@
-a = int(input("Enter the first nmuber: "))
-b = int(input("Enter the second number: "))
-if a>=b:
-    print(a," is maximum")
-else:
-    print(b," is maximum")
--- GeekForGeeks Programs/Basic Programs/14_sum_of_square_of_first_n_natural_number.py
+++
@@ -1,8 +0,0 @@
-def squaresum(n):
-    sm=0
-    for i in range(1,n+1):
-        sm=sm+(i*i)
-    return sm
-
-n=4
-print("Square of first n natural numbers are: ",squaresum(n))
